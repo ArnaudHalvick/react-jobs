83d466888903ac5f513a9b8fd9b656519faf83d2
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-01-28 16:59:44 -0300

    Put loading spinner higher

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import { ClipLoader } from "react-spinners";
 
 const Spinner = ({ loading }) => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center mt-20 h-screen">
       <ClipLoader color="#36d7b7" loading={loading} />
     </div>
   );
